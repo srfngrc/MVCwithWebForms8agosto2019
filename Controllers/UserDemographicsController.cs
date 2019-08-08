4cc19b8c28bf92e110f5e1b50308671a2a250a99
srfngrc <srfngrc@gmail.com>
2019-08-08 19:17:29 -0400

    chap06 exercise page181-185 finished ok. aug8th 7h17pm

---
+++ Controllers/UserDemographicsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCwithWebForms8agosto2019.Controllers
+{
+    public class UserDemographicsController : Controller
+    {
+        // GET: UserDemographics
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: UserDemographics/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserDemographics/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserDemographics/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserDemographics/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserDemographics/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserDemographics/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserDemographics/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
