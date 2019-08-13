da884ec599ecffa3814ac14d01ccf704278a5780
srfngrc <srfngrc@gmail.com>
2019-08-13 18:39:51 -0400

    excer page 256-257 finished. aug13th 6h39pm

---
+++ WeeklySpecial.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MVCwithWebForms8agosto2019
+{
+    public partial class WeeklySpecial : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int specialItemId = 24;
+            Response.Redirect("Items/Details/" + specialItemId);
+        }
+    }
+}
